91c7335b9dadae05a3bf33db69f1c68d95cd9f10
anas599 <anas599@yahoo.com>
2023-04-21 17:42:22 +0300

    Add fizzbuzz method to solver class

--- solver.rb
+++ solver.rb
@@ -6,4 +6,16 @@ class Solver
 
     (1..num).reduce(:*)
   end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
